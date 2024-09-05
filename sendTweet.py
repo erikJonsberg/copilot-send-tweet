0cef0a549a77f66a3ea483d920edeca0ac4d99c4
Erik Jonsberg <erikj@hungryghostdesign.com>
2024-09-05 13:28:05 -0400

    Sent a second tweet

--- sendTweet.py
+++ sendTweet.py
@@ -4,3 +4,21 @@ import tweepy
 # import the config file
 from config import *
 
+# create client with tweepy.Client with API key, API secret key, Access token, Access token secret
+
+client = tweepy.Client(
+    consumer_key = API_KEY,
+    consumer_secret = API_SECRET_KEY,
+    access_token = ACCESS_TOKEN,
+    access_token_secret = ACCESS_TOKEN_SECRET
+)
+
+# create a variable called tweet with a string that says 'I wrote this tweet with Copilot
+
+tweet2 = 'Tweeting from VS Code'
+
+# send tweet with client.create_tweet method and text as argument. Save the result in a variable called response
+
+response = client.create_tweet(text=tweet2)
+
+print(response)
